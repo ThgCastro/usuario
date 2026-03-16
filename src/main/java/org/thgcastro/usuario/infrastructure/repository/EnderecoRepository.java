9c23ad198389742279763381b214f422bc249460
Thiago Castro <nascimentocastro989@gmail.com>
2026-03-16 16:32:24 -0300

    security adicionado

---
+++ src/main/java/org/thgcastro/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package org.thgcastro.usuario.infrastructure.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.thgcastro.usuario.infrastructure.entity.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
