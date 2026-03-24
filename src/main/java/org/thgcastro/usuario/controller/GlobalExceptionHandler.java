4a8718c22835d2da85a4fd2a68b1ebd18ea1066c
Thiago <nascimentocastro989@gmail.com>
2026-03-24 12:59:41 -0300

    consulta de cep adicionado

--- src/main/java/org/thgcastro/usuario/controller/GlobalExceptionHandler.java
+++ src/main/java/org/thgcastro/usuario/controller/GlobalExceptionHandler.java
@@ -2,13 +2,14 @@ package org.thgcastro.usuario.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.thgcastro.usuario.infrastructure.exceptions.ConflictException;
 import org.thgcastro.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import org.thgcastro.usuario.infrastructure.exceptions.UnauthorizedException;
+import org.thgcastro.usuario.infrastructure.exceptions.IllegalArgumentException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
@@ -24,6 +25,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<String> handlerUnauthorizedException(UnauthorizedException exception){
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handlerIllegalArgumentException(IllegalArgumentException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
 }
--- src/main/java/org/thgcastro/usuario/controller/UsuarioController.java
+++ src/main/java/org/thgcastro/usuario/controller/UsuarioController.java
@@ -1,5 +1,7 @@
 package org.thgcastro.usuario.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,19 +9,25 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.thgcastro.usuario.business.UsuarioService;
+import org.thgcastro.usuario.business.ViaCepService;
 import org.thgcastro.usuario.business.dto.EnderecoDTO;
 import org.thgcastro.usuario.business.dto.TelefoneDTO;
 import org.thgcastro.usuario.business.dto.UsuarioDTO;
+import org.thgcastro.usuario.client.ViaCepDTO;
 import org.thgcastro.usuario.infrastructure.security.JwtUtil;
+import org.thgcastro.usuario.infrastructure.security.SecurityConfig;
 
 @RestController
 @RequestMapping("/usuario")
 @RequiredArgsConstructor
+@Tag(name = "Tarefas", description = "Cadastra tarefas de usuários")
+@SecurityRequirement(name = SecurityConfig.SECURITY_SCHEME)
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
+    private final ViaCepService viaCepService;
 
     @PostMapping
     public ResponseEntity<UsuarioDTO> savaUsuario(@RequestBody UsuarioDTO usuarioDTO){
@@ -73,5 +81,10 @@ public class UsuarioController {
     public ResponseEntity<TelefoneDTO> cadastraTelefone(@RequestBody TelefoneDTO dto,
                                                         @RequestHeader("Authorization") String token){
         return ResponseEntity.ok(usuarioService.cadastraTelefone(token, dto));
+    }
+
+    @GetMapping("/endereco/{cep}")
+    public ResponseEntity<ViaCepDTO> buscarDadosCep(@PathVariable String cep){
+        return ResponseEntity.ok(viaCepService.buscarDadosEndereco(cep));
     }
 }
